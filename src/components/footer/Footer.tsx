883924a154fda39d9869e80b05cec707ffcf5544
LH-TOFFANETTO <luizh.toffanetto@gmail.com>
2025-04-28 19:37:15 -0300

    Adicionando pastas e arquivos do projeto original

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,25 @@
+import { Instagram } from "lucide-react";
+
+export default function Footer() {
+    return (
+        <>
+            <footer className="w-full pt-8 p-2 bg-off-white dark:bg-midnight-blue border-t border-midnight-blue dark:border-blue-darknut text-center">
+                <a className="text-sky-500">
+                    hub@uem.br
+                </a>
+                <p className="my-5 leading-normal">
+                    Universidade Estadual de Maringá
+                    <br />
+                    Bloco C23, Sala 3
+                    <br />
+                    Maringá, PR, Brasil
+                </p>
+                <nav>
+                    <a className="inline-flex items-center hover:text-blue-darknut">
+                        <Instagram />
+                    </a>
+                </nav>
+            </footer>
+        </>
+    );
+}
---
+++ src/redux/features/questionnaireApiSlice.ts
@@ -0,0 +1,53 @@
+import { apiSlice } from '../services/apiSlice';
+
+interface Question {
+    id: number;
+    pergunta: string;
+    explicacao: string;
+}
+
+interface Dimensao {
+    id: number;
+    dimensaoTitulo: string;
+    descricao: string;
+    tipo: 'OBRIGATORIO' | 'COMERCIO' | 'SERVICO' | 'INDUSTRIA';
+    perguntas: Question[];
+}
+
+interface Modulo {
+    id: number;
+    nome: string;
+    descricao: string;
+    perguntasQntd: number;
+    tempo: number;
+    dimensoes: Dimensao[];
+}
+
+interface RespostaModulo {
+    perguntaId: number;
+    valor: number;
+}
+
+export const questionnaireApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getQuestionnaires: builder.query<Dimensao[], void>({
+            query: () => '/questionario/',
+        }),
+        getQuestionnaireByModule: builder.query<Modulo, string>({
+            query: (nomeModulo: string) => `/questionario/modulos/${nomeModulo}/`,
+        }),
+        saveModuleResponses: builder.mutation<void, { nomeModulo: string; respostas: RespostaModulo[] }>({
+            query: ({ nomeModulo, respostas }) => ({
+                url: `/modulos/${nomeModulo}/respostas/`,
+                method: 'POST',
+                body: { respostas },
+            }),
+        }),
+    }),
+});
+
+export const {
+    useGetQuestionnairesQuery,
+    useGetQuestionnaireByModuleQuery,
+    useSaveModuleResponsesMutation,
+} = questionnaireApiSlice;
